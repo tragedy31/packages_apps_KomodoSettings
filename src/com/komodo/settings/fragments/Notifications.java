035299cccb5415ed721bb449d45ab3663373e38d
Anushek Prasal <anushekprasal@gmail.com>
2019-12-03 00:44:16 +0530

    Add ambient pulse notification [2/2]
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>
    Change-Id: I9fd6f450d1ed854ecefca39e10e3683f343c1155
    Signed-off-by: goodmeow <harunbam3@gmail.com>

--- src/com/komodo/settings/fragments/Notifications.java
+++ src/com/komodo/settings/fragments/Notifications.java
@@ -24,11 +24,12 @@ import android.os.Bundle;
 import android.provider.Settings;
 import androidx.preference.*;
 
-import com.android.internal.logging.nano.MetricsProto; 
+import com.android.internal.logging.nano.MetricsProto;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
 import com.komodo.settings.preferences.CustomSeekBarPreference;
 import com.komodo.settings.preferences.GlobalSettingMasterSwitchPreference;
 
@@ -37,8 +38,10 @@ public class Notifications extends SettingsPreferenceFragment
 
     public static final String TAG = "Notifications";
     private static final String HEADS_UP_NOTIFICATIONS_ENABLED = "heads_up_notifications_enabled";
+    private static final String PULSE_AMBIENT_LIGHT_COLOR = "pulse_ambient_light_color";
 
     private GlobalSettingMasterSwitchPreference mHeadsUpEnabled;
+    private ColorPickerPreference mEdgeLightColorPreference;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -52,6 +55,18 @@ public class Notifications extends SettingsPreferenceFragment
         int headsUpEnabled = Settings.Global.getInt(getContentResolver(),
                 HEADS_UP_NOTIFICATIONS_ENABLED, 1);
         mHeadsUpEnabled.setChecked(headsUpEnabled != 0);
+        mEdgeLightColorPreference = (ColorPickerPreference) findPreference(PULSE_AMBIENT_LIGHT_COLOR);
+        int edgeLightColor = Settings.System.getInt(getContentResolver(),
+                Settings.System.PULSE_AMBIENT_LIGHT_COLOR, 0xFF3980FF);
+        mEdgeLightColorPreference.setNewPreviewColor(edgeLightColor);
+        mEdgeLightColorPreference.setAlphaSliderEnabled(false);
+        String edgeLightColorHex = String.format("#%08x", (0xFF3980FF & edgeLightColor));
+        if (edgeLightColorHex.equals("#ff3980ff")) {
+            mEdgeLightColorPreference.setSummary(R.string.default_string);
+        } else {
+            mEdgeLightColorPreference.setSummary(edgeLightColorHex);
+        }
+        mEdgeLightColorPreference.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -66,7 +81,19 @@ public class Notifications extends SettingsPreferenceFragment
             Settings.Global.putInt(getContentResolver(),
 		            HEADS_UP_NOTIFICATIONS_ENABLED, value ? 1 : 0);
             return true;
-        }
+        } else if (preference == mEdgeLightColorPreference) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(objValue)));
+            if (hex.equals("#ff3980ff")) {
+                preference.setSummary(R.string.default_string);
+            } else {
+                preference.setSummary(hex);
+            }
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.PULSE_AMBIENT_LIGHT_COLOR, intHex);
+            return true;
+	}
         return false;
     }
 
