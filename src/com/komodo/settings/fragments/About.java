1ecb5da4f1c24015f05ee01b07380d52e2978ca6
Kry9toN <dhimasbagusprayoga@gmail.com>
2020-02-05 15:07:26 +0700

    Komodopannel: add about komodo
    Signed-off-by: kry9ton <dhimasbagusprayoga@gmail.com>

--- src/com/komodo/settings/fragments/About.java
+++ src/com/komodo/settings/fragments/About.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Havoc-OS
+ * Copyright (C) 2019 Komodo OS
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,7 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.provider.Settings;
@@ -30,19 +31,30 @@ import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class About extends SettingsPreferenceFragment implements
-Preference.OnPreferenceChangeListener {
-
-    @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-        addPreferencesFromResource(R.xml.komodo_settings_about);
-        PreferenceScreen prefSet = getPreferenceScreen();
-    }
-
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return false;
+public class About extends SettingsPreferenceFragment {
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.komodo_settings_about);
+		PreferenceCategory maintainers = (PreferenceCategory)findPreference("maintainers");
+		String[] maintainers_title = getResources().getStringArray(R.array.maintainers_title);
+        String[] maintainers_devices = getResources().getStringArray(R.array.maintainers_devices);
+        String[] maintainers_url = getResources().getStringArray(R.array.maintainers_url);
+		for (int i = 0; i < maintainers_title.length; i++) {
+            Preference maintainer = new Preference(getPrefContext());
+            final String maintainer_url = maintainers_url[i];
+            maintainer.setIcon(R.drawable.ic_devs_phone);
+            maintainer.setTitle(maintainers_title[i]);
+            maintainer.setSummary(String.format(getString(R.string.maintainer_description), maintainers_devices[i]));
+            maintainer.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+					@Override
+					public boolean onPreferenceClick(Preference preference) {
+						getActivity().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(maintainer_url)));
+						return true;
+					}
+				});
+            maintainers.addPreference(maintainer);
+        }
     }
 
     @Override
